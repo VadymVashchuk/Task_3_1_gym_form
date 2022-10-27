8fda4732f301d137409dc1a11c13f3b4e24e2bae
VadymVashchuk <114853541+VadymVashchuk@users.noreply.github.com>
2022-10-27 20:56:43 +0300

    improved_js_code_and_add_some_new_conditions

--- script.js
+++ script.js
@@ -12,21 +12,23 @@ function registration () {
   let weightValue = clientWeight.value;
   let heightValue = clientHeight.value;
 
+  let baseErrorMessage = ", ми змушені повідомити, що Ви не зможете відвідувати наш зал"
+
   if (nameValue == 0 || ageValue == 0 || weightValue == 0 || heightValue == 0) {
     alert("Будь ласка, заповніть форму до кінця!");
+    return;
   } else if (ageValue < 18 || ageValue > 60) {
-    alert("Зважаючи на Ваш вік, ми змушені повідомити, що Ви не зможете відвідувати наш зал");
-    clearAll ();
+    alert("Зважаючи на Ваш вік" + baseErrorMessage);
   } else if (weightValue < 60 || weightValue > 120) {
-    alert("Зважаючи на Вашу вагу, ми змушені повідомити, що Ви не зможете відвідувати наш зал");
-    clearAll ();
+    alert("Зважаючи на Вашу вагу" + baseErrorMessage);
   } else if (heightValue < 160 || heightValue > 195) {
-    alert("Зважаючи на Ваш зріст, ми змушені повідомити, що Ви не зможете відвідувати наш зал");
-    clearAll ();
+    alert("Зважаючи на Ваш зріст" + baseErrorMessage);
+  } else if (heightValue - weightValue > 130 || heightValue - weightValue < 70) {
+    alert("Зважаючи на співвідношення Вашої ваги та зросту" + baseErrorMessage);
   } else {
     alert("Вітаємо, " + nameValue + "! Ви зареєстровані.");
-    clearAll ();
   }
+  clearAll ();
 }
 
 function clearAll () {
